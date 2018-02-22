c3c84104674a427a214a5d42d5add146fdb14b52
pasudo123 <pasudo123@naver.com>
2018-02-22 17:16:00 +0900

    하이레벨 클래스 업데이트

--- DatabasePerfomance/src/com/pasudo/main/Main.java
+++ DatabasePerfomance/src/com/pasudo/main/Main.java
@@ -1,73 +1,90 @@
 package com.pasudo.main;
 
+import com.pasudo.database.QueryCollection;
 import com.pasudo.submain.Integration;
+import com.pasudo.submain.IntegrationImpl;
 import com.pasudo.submain.PerformTimer;
 
 public class Main {
 	private static Integration integration = null;
 	private static PerformTimer performTimer = null;
+	private static EnumParseFile TYPE_PARSE = null;
+	private static EnumDatabase TYPE_DB = null;
+	private static String tableName = null;
+	
 	
 	// 데이터베이스와 파싱 객체를 담고있는 통합 객체 획득
 	private static Integration getIntegration(){
-		Integration integration = new Integration();
-		
+		Integration integration = new IntegrationImpl();
 		return integration;
 	}
 		
 	public static void main(String[]args){
 		integration = getIntegration();
-//		settingIntegration(integration);
+		settingParseMaker();
+		settingDB();
 		
 		//-- 타이머 생성 & 시간 측정 (시작)
 		settingPerformTimer();
 		performTimer.start();
 		
-//		convertDatabase2File(integration);
-		convertFile2Database(integration);
+		// INSERT 테이블 결정 & SELECT 테이블 결정
+		// 해당 코드도 결국 QueryCollection 이 변경되면 변경될 수 밖에 없는 구조를 가지고 있다.
+		tableName = "PASUDO_DOC";
+		QueryCollection.addTableNameOnInsert(tableName);
+		QueryCollection.addOrderByAscOnSelect(tableName);
+		
+		convertDatabase2File();
+		convertFile2Database();
 		
 		//-- 시간 측정 (종료)
 		performTimer.end();
 	}
 	
-	// 통합객체를 통해서 데이터베이스와 파서 세팅
-	private static void settingIntegration(Integration integration){
-		/**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-		 *  [ Database Connector 설정 ]
-		 **ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ**/
-//		integration.setOracleLocalConnector();
-		integration.setOracleRemoteConnector();
-		
+	private static void settingParseMaker(){
 		/**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 		 * 		  [ Parser 설정 ]
 		 **ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ**/
-//		integration.setCsvParserMaker();
-//		integration.setJsonParseMaker();
-//		integration.setTaggedFormatParseMaker();
-		integration.setTsvParseMaker();
+//		TYPE_PARSE = EnumParseFile.TSV;
+//		TYPE_PARSE = EnumParseFile.TAGGED_FORMAT;
+//		TYPE_PARSE = EnumParseFile.JSON;
+//		TYPE_PARSE = EnumParseFile.CSV;
+		
+		integration.setParseMaker(TYPE_PARSE);
 	}
 	
-	private static void settingPerformTimer(){
-		performTimer = new PerformTimer();
+	private static void settingDB(){
+		/**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 *  [ Database Connector 설정 ]
+		 **ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ**/
+//		TYPE_DB = EnumDatabase.ORACLE_LOCAL;
+//		TYPE_DB = EnumDatabase.ORACLE_REMOTE;
+//		TYPE_DB = EnumDatabase.MYSQL_LOCAL;
+		
+		integration.setConnectorMaker(TYPE_DB);
 	}
 	
 	// 파일을 데이터베이스로 변환
-	private static void convertFile2Database(Integration integration){
-		integration.inputDatabase();
+	private static void convertFile2Database(){
+		integration.file2Database();
 	}
 	
 	// 데이터베이스를 파일로 변환
-	private static void convertDatabase2File(Integration integration){
-		
+	private static void convertDatabase2File(){
+
 		/**
 		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 		 * 
-		 * 오름차순 : +1
-		 * 내림차순 : -1
-		 * 그외경우 :  0
+		 * -- [ 정렬 기준  ] : DOC_SEQ // TITLE // REG_DT
+		 * -- [ 오름차순 or 내림차순  ]
 		 * 
 		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 		 * **/
 		
-		integration.outputDatabase("REG_DT", -1);
+		integration.database2File("REG_DT", -1);
+	}
+	
+	private static void settingPerformTimer(){
+		performTimer = new PerformTimer();
 	}
 }
