c8832a2fbfa12a179c3e1c50f2061c2c5061632a
pasudo123 <pasudo123@naver.com>
2018-02-22 17:14:19 +0900

    디비 인터페이스 및 클래스 의존관계 리팩토링 및 쿼리문 수정

--- DatabasePerfomance/src/com/pasudo/database/OracleRemoteConnector.java
+++ DatabasePerfomance/src/com/pasudo/database/OracleRemoteConnector.java
@@ -5,7 +5,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.pasudo.parser.EnumDocName;
 
 
 /**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
@@ -18,10 +21,11 @@ public class OracleRemoteConnector implements ConnectionMaker{
 
 	private Connection connection = null;
 	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
 	
-	/** 현재에는 쓰이지 않음 **/
-//	private ResultSet resultSet = null;
-//	private List<String[]> resultAllRowsData = null;
+	private String DOC_SEQ = EnumDocName.COLUMN_HEADER_DOC_SEQ.getName();
+	private String TITLE = EnumDocName.COLUMN_HEADER_DOC_TITLE.getName();
+	private String REG_DT = EnumDocName.COLUMN_HEADER_DOC_REG_DT.getName();
 	
 	public OracleRemoteConnector(){
 		connection = ConnectionMaker.decisionDatabase(this);
@@ -30,54 +34,51 @@ public class OracleRemoteConnector implements ConnectionMaker{
 	@Override
 	public void insertDatabase(List<String[]> allRowsData) {
 		int size = allRowsData.size();
+		String query = null;
 		
-		/** Batch START  **/
-		batchInsertDatabase();
-		String query = "INSERT INTO PASUDO_DOC_COPY VALUES(?, ?, ?)";
-		try {
+		/**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 * 
+		 *  	     [ jdbc batch ]  
+		 * 
+		 *ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ**/
+		
+		try{
+			connection.setAutoCommit(false);
+			query = QueryCollection.getInsertQuery();
 			preparedStatement = connection.prepareStatement(query);
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
+		} 
+		catch (SQLException e1) {
 			e1.printStackTrace();
 		}
-		/** Batch START  **/
 		
+		/** add PreparedStatement batch **/
 		// 라인 : 0번째는 해당 칼럼의 헤더가 있기 때문에 생략 (TSV 기준)
 		for(int line = 1; line < size; line++){
 			String[]data = allRowsData.get(line);
 			executeInsertQuery(data[0], data[1], data[2]);
 
 			if(line % 10000 == 0)
-//				System.out.println(data[0] + ", " + data[1] + ", " +  data[2]);
 				System.out.println(size + " / " + line);
 			
 			/** add PreparedStatement batch **/
 			try {
 				preparedStatement.addBatch();
 				preparedStatement.clearParameters();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+			} 
+			catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}// for
 		
 		
-		// connection >> close
 		try {
 			/** Batch END **/
 			preparedStatement.executeBatch();
 			connection.commit();
+			/** Batch END **/
 			
+			preparedStatement.close();
 			connection.close();
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void batchInsertDatabase(){
-		try {
-			connection.setAutoCommit(false);
 		} 
 		catch (SQLException e) {
 			e.printStackTrace();
@@ -90,60 +91,54 @@ public class OracleRemoteConnector implements ConnectionMaker{
 			preparedStatement.setInt(1, Integer.parseInt(DOC_SEQ));
 			preparedStatement.setString(2, TITLE);
 			preparedStatement.setString(3, REG_DT);
-			
-//			preparedStatement.executeUpdate();
 		} 
 		catch (SQLException e) {
-			System.out.println("OracleLocalConnector : SQLException");
+			System.out.println("Oracle [Remote] Connector : SQLException");
 			System.out.println(DOC_SEQ + ", " + TITLE + ", " + REG_DT);
 			e.printStackTrace();
 		}
-		finally{
-//			// statement >> close
-//			if(preparedStatement != null)
-//				try {
-//					preparedStatement.close();
-//				} 
-//				catch (SQLException e) {
-//					e.printStackTrace();
-//				}
+	}
+
+	@Override
+	public List<String[]> selectDatabase(String sortCase, Integer order) {
+		if(sortCase.equals(DOC_SEQ) || sortCase.equals(TITLE) || sortCase.equals(REG_DT)){
+			if(order ==  1)
+				QueryCollection.addOrderByAscOnSelect(sortCase);
+			if(order == -1)
+				QueryCollection.addOrderByDescOnSelect(sortCase);
 		}
+			
+		return executeSelectQuery(QueryCollection.getSelectQuery());
 	}
 
 	@Override
-	public List<String[]> selectDatabase(String sortCase, int flag) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<String[]> executeSelectQuery() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<String[]> executeSelectQueryBySortingOnDOC_SEQ(int flag) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<String[]> executeSelectQueryBySortingOnTITLE(int flag) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<String[]> executeSelectQueryBySortingOnREG_DT(int flag) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String[]> executeSelectQuery(String paramQuery) {
+		String query = paramQuery;
+		return resultSetProcess(query);
 	}
 
 	@Override
 	public List<String[]> resultSetProcess(String query) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String[]> allRowsData = new ArrayList<String[]>();
+		
+		try {
+			// 조회 쿼리
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+			
+			while(resultSet.next()){
+				String docSeq = String.valueOf(resultSet.getInt(DOC_SEQ));
+				String title = resultSet.getString(TITLE);
+				String registerDate = resultSet.getString(REG_DT);
+				
+				String[] rowDatas = {docSeq, title, registerDate};
+				allRowsData.add(rowDatas);
+			}
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return allRowsData;
 	}
-
 }
