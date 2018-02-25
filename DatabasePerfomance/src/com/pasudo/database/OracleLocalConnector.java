4fe61d18924fd2223598e1f27b40aad7268639fc
pasudo123 <pasudo123@naver.com>
2018-02-25 12:41:40 +0900

    오라클 내부 preparedStatement batch 형태로 수정

--- DatabasePerfomance/src/com/pasudo/database/OracleLocalConnector.java
+++ DatabasePerfomance/src/com/pasudo/database/OracleLocalConnector.java
@@ -27,19 +27,59 @@ public class OracleLocalConnector implements ConnectionMaker{
 	@Override
 	public void insertDatabase(List<String[]> allRowsData) {
 		int size = allRowsData.size();
+		String query = null;
+		int counter = 1;
+		
+		/**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 * 
+		 *  	     [ jdbc batch ]  
+		 * 
+		 *ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ**/
+		
+		try{
+			// 기본적으로 커밋의 자동수행을 false 로 변경한다.( 기본이 true 이기 때문에 )
+			connection.setAutoCommit(false);
+			query = QueryCollection.getInsertQuery();
+			preparedStatement = connection.prepareStatement(query);
+		} 
+		catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 		
 		// 라인 : 0번째는 해당 칼럼의 헤더가 있기 때문에 생략 (TSV 기준)
 		for(int line = 1; line < size; line++){
 			String[]data = allRowsData.get(line);
 			executeInsertQuery(data[0], data[1], data[2]);
 			
-			if(line % 10000 == 0)
-//				System.out.println(data[0] + ", " + data[1] + ", " +  data[2]);
-				System.out.println(size + " / " + line);
+			try {
+				/** add PreparedStatement batch **/
+				if(line % 1000 == 0){
+					preparedStatement.executeBatch();
+					connection.commit();
+					
+					System.out.println("Batch "+ (counter++) +" executed successfully");
+					System.out.println(size + " / " + line);
+				}
+				
+				// executeUpdate() 메소드 대신에 addBatch() 메소드 실행 
+				// addBatch() : 쿼리와 파라미터들을 배치에 추가 이후 executeBatch() 실행
+				preparedStatement.addBatch();
+			} 
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+				
 		}// for
 		
 		// connection >> close
 		try {
+			/** Batch END **/
+			// executeBatch() 메소드를 수행해서 한번에 쿼리를 수행한다.
+			preparedStatement.executeBatch();
+			connection.commit();
+			/** Batch END **/
+			
+			preparedStatement.close();
 			connection.close();
 		} 
 		catch (SQLException e) {
@@ -48,32 +88,17 @@ public class OracleLocalConnector implements ConnectionMaker{
 	}
 	
 	public void executeInsertQuery(String DOC_SEQ, String TITLE, String REG_DT){
-		String query = QueryCollection.getInsertQuery();
-		
 		try {
-			preparedStatement = connection.prepareStatement(query);
-			
 			preparedStatement.setInt(1, Integer.parseInt(DOC_SEQ));
 			preparedStatement.setString(2, TITLE);
 			preparedStatement.setString(3, REG_DT);
 			
-			preparedStatement.executeUpdate();
+//			preparedStatement.executeUpdate();
 		} 
 		catch (SQLException e) {
 			System.out.println("Oracle [Local] Connector : SQLException");
 			System.out.println(DOC_SEQ + ", " + TITLE + ", " + REG_DT);
 			e.printStackTrace();
-		}
-		finally{
-			// statement >> close
-			if(preparedStatement != null){
-				try {
-					preparedStatement.close();
-				} 
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 	
