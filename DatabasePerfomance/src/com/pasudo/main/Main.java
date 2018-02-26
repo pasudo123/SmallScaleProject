eccf03cf86ed6d7b73e170253141e9efd0820030
pasudo123 <pasudo123@naver.com>
2018-02-26 22:04:46 +0900

    Update Console Print UI

--- DatabasePerfomance/src/com/pasudo/main/Main.java
+++ DatabasePerfomance/src/com/pasudo/main/Main.java
@@ -1,5 +1,7 @@
 package com.pasudo.main;
 
+import java.util.Map;
+
 import com.pasudo.database.QueryCollection;
 import com.pasudo.submain.Integration;
 import com.pasudo.submain.IntegrationImpl;
@@ -21,9 +23,15 @@ public class Main {
 	
 		
 	public static void main(String[]args){
+		// 출력 내용
+		Print print = new Print();
+		print.initPrint();
+		Map<String, Integer> resultMap = print.start();
+		
+		// 통합 객체 및 파싱 및 디비 세팅
 		integration = getIntegration();
-		settingParseMaker();
-		settingDB();
+		settingParseMaker(resultMap.get("file"));
+		settingDB(resultMap.get("db"));
 		
 		//-- 타이머 생성 & 시간 측정 (시작)
 		settingPerformTimer();
@@ -35,32 +43,52 @@ public class Main {
 		QueryCollection.addTableNameOnInsert(tableName);
 		QueryCollection.addTableNameOnSelect(tableName);
 
-//		convertFile2Database();
-		convertDatabase2File();
+		if(resultMap.get("convert") == 1)
+			convertDatabase2File();
+		else
+			convertFile2Database();
 		
 		//-- 시간 측정 (종료)
 		performTimer.end();
 	}
 	
-	private static void settingParseMaker(){
+	private static void settingParseMaker(int choice){
 		/**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 		 * 		  [ Parser 설정 ]
 		 **ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ**/
-		TYPE_PARSE = EnumParseFile.TSV;
-//		TYPE_PARSE = EnumParseFile.TAGGED_FORMAT;
-//		TYPE_PARSE = EnumParseFile.JSON;
-//		TYPE_PARSE = EnumParseFile.CSV;
+		switch(choice){
+		case 1:
+			TYPE_PARSE = EnumParseFile.CSV;
+			break;
+		case 2:
+			TYPE_PARSE = EnumParseFile.TSV;
+			break;
+		case 3:
+			TYPE_PARSE = EnumParseFile.TAGGED_FORMAT;
+			break;
+		case 4:
+			TYPE_PARSE = EnumParseFile.JSON;
+			break;
+		}
 		
 		integration.setParseMaker(TYPE_PARSE);
 	}
 	
-	private static void settingDB(){
+	private static void settingDB(int choice){
 		/**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 		 *  [ Database Connector 설정 ]
 		 **ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ**/
-//		TYPE_DB = EnumDatabase.ORACLCE_LOCAL;
-		TYPE_DB = EnumDatabase.ORACLE_REMOTE;
-//		TYPE_DB = EnumDatabase.MYSQL_LOCAL;
+		switch(choice){
+		case 1:
+			TYPE_DB = EnumDatabase.ORACLE_LOCAL;
+			break;
+		case 2:
+			TYPE_DB = EnumDatabase.ORACLE_REMOTE;
+			break;
+		case 3:
+			TYPE_DB = EnumDatabase.MYSQL_LOCAL;
+			break;
+		}
 		
 		integration.setConnectorMaker(TYPE_DB);
 	}
@@ -88,7 +116,7 @@ public class Main {
 		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 		 * **/
 
-		integration.database2File("DOC_SEQ", 1);
+		integration.database2File("DOC_SEQ", -1);
 	}
 	
 	private static void settingPerformTimer(){
