26825212237bc70ce6079eaf210bc198bbc24c6a
pasudo123 <pasudo123@naver.com>
2018-03-01 21:04:21 +0900

    서비스 관련 레이어 추가

---
+++ CpcCategorize/src/main/java/edu/doubler/service/CpcService.java
@@ -0,0 +1,24 @@
+package edu.doubler.service;
+
+import java.util.List;
+
+import edu.doubler.dao.CpcDto;
+
+public interface CpcService {
+	
+	// [ 전체 CPC 섹션 획득 ]
+	public List<CpcDto> selectCpcSection();
+	
+	// [ 해당 코드를 통해 CPC 데이터 획득  ]
+	public CpcDto selectCpcData(String code);
+	
+	// [ 섹션을 통해 클래스 획득  ]
+	public List<CpcDto> selectClassBySection(String sectionName);
+	
+	// [ 클래스를 통해 서브 클래스 획득 ]
+	public List<CpcDto> selectSubClassByClass(String className);
+	
+	// [ 서브 클래스를 통해 하위 목록 획득 ]
+	public List<CpcDto> selectChildBySubClass(String subClassName);
+	
+}
---
+++ CpcCategorize/src/main/java/edu/doubler/service/CpcServiceImpl.java
@@ -0,0 +1,110 @@
+package edu.doubler.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.doubler.dao.CpcDao;
+import edu.doubler.dao.CpcDto;
+import edu.doubler.domain.EnumSection;
+
+@Service
+public class CpcServiceImpl implements CpcService{
+
+	@Autowired
+	private CpcDao cpcDao;
+	private List<CpcDto> sectionData = null;
+	private List<CpcDto> classData = null;
+	private List<CpcDto> subClassData = null;
+	
+
+	@Override
+	public List<CpcDto> selectCpcSection() {
+		/**
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 * 
+		 * 전체 CPC 섹션 획득
+		 * 
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 **/
+		
+		return cpcDao.selectCpcSection();
+	}
+
+	
+	@Override
+	public CpcDto selectCpcData(String code) {
+		/**
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 * 
+		 * 코드를 통해 CPC 데이터 획득
+		 * 
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 **/
+		String parseCode = parseUrlName(code);
+
+		return cpcDao.selectCpcData(parseCode);
+	}
+
+	
+	@Override
+	public List<CpcDto> selectClassBySection(String sectionName) {
+		/**
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 * 
+		 * 섹션을 통해서 클래스 내용을 추출
+		 * 
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 **/
+
+		sectionName = parseUrlName(sectionName);
+		
+		EnumSection[] enumSection = EnumSection.values();
+		for(EnumSection sectionType : enumSection){
+			if(sectionType.getSectionName().equals(sectionName)){
+				sectionData = cpcDao.selectClassBySection(sectionName);
+				
+				return sectionData;
+			}
+		}
+		
+		return null;
+	}
+
+	@Override
+	public List<CpcDto> selectSubClassByClass(String className) {
+		/**
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 * 
+		 * 클래스를 통해서 서브 클래스 추출
+		 * 
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 **/
+		
+		classData = cpcDao.selectSubClassByClass(className);
+
+		return classData;
+	}
+
+	@Override
+	public List<CpcDto> selectChildBySubClass(String subClassName) {
+		/**
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 * 
+		 * 서브 클래스를 통해 하위 목록 추출
+		 * 
+		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		 **/
+		
+		subClassData = cpcDao.selectChildBySubClass(subClassName);
+		
+		return subClassData;
+		
+	}
+	
+	// 들어오는 값에 대한 URL 전처리
+	private String parseUrlName(String name) {
+		return name.replaceAll("/", "");
+	}
+}
