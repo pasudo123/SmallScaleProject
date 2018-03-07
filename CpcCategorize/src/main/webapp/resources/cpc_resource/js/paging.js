826ddf1e846a4f9659b5ed733f5cf34dc9fc3b9b
pasudo123 <pasudo123@naver.com>
2018-03-07 20:39:22 +0900

    js 및 페이징 부분 처리 ROWNUM 및 서브쿼리 이용

---
+++ CpcCategorize/src/main/webapp/resources/cpc_resource/js/paging.js
@@ -0,0 +1,21 @@
+//$(document).ready(function(){
+//	$('.classWrapper').on('click', 'a.pagingBtn', function(){
+//		var pageId = $(this).attr('id');
+//		var url = $(location).attr('href');
+//		
+//		$.ajax({
+//			type:'POST',
+//			url:url + '/page',
+//			data:{page : pageId},
+//			dataType:'html',
+//			
+//			success:function(data){
+//				$('div.classWrapper').html(data);
+//			},
+//			error:function(request, status, error){
+//				alert(error);
+//			}
+//		});
+//	});
+//});
+ 
