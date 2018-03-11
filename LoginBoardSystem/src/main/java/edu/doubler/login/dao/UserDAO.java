ab4d973a17e7d00f217df3eeb8bdd999c5ab03aa
pasudo123 <pasudo123@naver.com>
2018-03-11 23:01:18 +0900

    유저 로그인 비즈니스 로직 및 DAO 코드 업데이트

--- LoginBoardSystem/src/main/java/edu/doubler/login/dao/UserDAO.java
+++ LoginBoardSystem/src/main/java/edu/doubler/login/dao/UserDAO.java
@@ -1,5 +1,9 @@
 package edu.doubler.login.dao;
 
-public interface UserDAO {
+import edu.doubler.login.domain.AppleUser;
 
+public interface UserDAO {
+	
+	// id, pw 체크
+	public Object checkUser(AppleUser appleUser);
 }
--- LoginBoardSystem/src/main/java/edu/doubler/login/dao/UserDAOImpl.java
+++ LoginBoardSystem/src/main/java/edu/doubler/login/dao/UserDAOImpl.java
@@ -1,5 +1,20 @@
 package edu.doubler.login.dao;
 
-public class UserDAOImpl {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import edu.doubler.login.domain.AppleUser;
+
+@Repository
+public class UserDAOImpl implements UserDAO{
+
+	@Autowired
+	SqlSession sqlSession;
+	private final String NAME_SPACE = "edu.doubler.login.dao.userDAO";
+	
+	@Override
+	public Object checkUser(AppleUser appleUser) {
+		return sqlSession.selectOne(NAME_SPACE + ".checkUser", appleUser);
+	}
 }
