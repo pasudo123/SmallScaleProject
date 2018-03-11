15518ef7dc18dfc5f1c15568998d73e79f546198
pasudo123 <pasudo123@naver.com>
2018-03-11 23:00:57 +0900

    컨트롤러 쪽 업데이트

--- LoginBoardSystem/src/main/java/edu/doubler/login/controller/LoginController.java
+++ LoginBoardSystem/src/main/java/edu/doubler/login/controller/LoginController.java
@@ -1,37 +1,41 @@
 package edu.doubler.login.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import edu.doubler.login.domain.EnumDomainName;
+import edu.doubler.login.domain.AppleUser;
 import edu.doubler.login.service.LoginService;
 
 @Controller
 public class LoginController {
 	
+	@Autowired 
+	LoginService loginService;
+	
 	@RequestMapping(value="/")
-	public String login(HttpServletRequest request){
+	public String Launcher(Model model){
 		
-//		아래와 같은 주소가 있을 경우
-//		http://localhost:8080/test/index.jsp
-//		request.getRequestURI();   //프로젝트경로부터 파일까지의 경로값을 얻어옴 (/test/index.jsp)
-//		request.getContextPath();  //프로젝트의 경로값만 가져옴(/test)
-//		request.getRequestURL();   //전체 경로를 가져옴 (http://localhost:8080/test/index.jsp)
-//		request.getServletPath();  //파일명 (/index.jsp)
+		// 도메인 객체 
+		model.addAttribute("AppleUser", new AppleUser());
 		
-		int portNumber = request.getServerPort();
+		return "/login_view/login_form_apple";
+	}
+	
+	@RequestMapping(value="/login")
+	public String logIn(
+	@ModelAttribute("AppleUser") AppleUser appleUser){
 		
-		switch(portNumber){
-		case 8181:
-			return EnumDomainName.APPLE.getViewPath();
-		case 8180:
-			return EnumDomainName.BANANA.getViewPath();
-		default:
-			return null;
-		}
+		boolean check = loginService.checkUser(appleUser);
+		
+		// check = true
+		if(check)
+			return "board_view/board_list_screen";
+		
+		return "/login_view/login_form_apple";
 	}
 }
