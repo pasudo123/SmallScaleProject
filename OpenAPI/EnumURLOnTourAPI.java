5ff3630a8d4d811a91937ba2d6245460be96e639
pasudo123 <pasudo123@naver.com>
2018-03-17 22:03:34 +0900

    openAPI 사용 코드 추가(TourAPI)

---
+++ OpenAPI/EnumURLOnTourAPI.java
@@ -0,0 +1,26 @@
+package OpenAPI;
+
+public enum EnumURLOnTourAPI {
+	
+	/**ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+	 * 
+	 * [ 국문 관광정보 서비스 API ] 
+	 * 
+	 * @since 2018 03 17
+	 * @author doubleR
+	 * @category mashup service : url
+	 * @ses https://www.data.go.kr/dataset/15000496/openapi.do
+	 * 
+	 * - 활용 사례명 : doublerTour
+	 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ**/
+	SELECT_AREA_CODE("http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode"),
+	SELECT_AREA_BASED_LIST("http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList");
+	
+	private String url = null;
+	
+	private EnumURLOnTourAPI(String url){
+		this.url = url;
+	}
+	
+	public String getURL(){ return url; }
+}
