cec53bb57bb7b85c4933f8fafd2c07868c9b681e
pasudo123 <pasudo123@naver.com>
2018-03-22 22:01:10 +0900

    pageNo 를 파라미터로 받고 api 호출 코드 업데이트

--- ILoveSeoul/src/main/java/edu/doubler/menu/service/MenuService.java
+++ ILoveSeoul/src/main/java/edu/doubler/menu/service/MenuService.java
@@ -1,12 +1,14 @@
 package edu.doubler.menu.service;
 
+import java.util.LinkedHashMap;
 import java.util.List;
-
-import edu.doubler.domain.AreaBasedData;
-import edu.doubler.domain.LocationBasedData;
 
 public interface MenuService {
 	
 	// 위치기반으로 관광정보 조회 (위도, 경도, 반경)
-	public List<Object> selectListOnLocationBasedInfo(String mapX, String mapY, String radius);
+	public List<Object> selectListOnLocationBasedInfo(String mapX, String mapY, String radius, String pageNo);
+
+	
+	// Responsebody 를 위한 json 데이터 획득
+	public LinkedHashMap<String, Object> selectPageMovement(String mapX, String mapY, String radius, String pageNo);
 }
--- ILoveSeoul/src/main/java/edu/doubler/menu/service/MenuServiceImpl.java
+++ ILoveSeoul/src/main/java/edu/doubler/menu/service/MenuServiceImpl.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -21,10 +20,8 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import edu.doubler.domain.AreaBasedData;
 import edu.doubler.domain.LocationBasedData;
 import edu.doubler.enum_api.EnumParameterOnTourAPI;
-import edu.doubler.enum_api.EnumSigunguCode;
 import edu.doubler.enum_api.EnumURLOnTourAPI;
 
 @Service
@@ -33,25 +30,11 @@ public class MenuServiceImpl implements MenuService {
 	private static final Logger logger = LoggerFactory.getLogger(MenuServiceImpl.class);
 	private ObjectMapper objectMapper;
 	private byte[] mapDatas;
-
+	
+	// 주변 지역 살피기
 	@Override
-	public List<Object> selectListOnLocationBasedInfo(String mapX, String mapY, String radius) {
-		String url = batchSettingUrl(EnumURLOnTourAPI.SELECT_LOCATION_BASED_LIST);
-
-		// 위도, 경도, 반경 설정
-		EnumParameterOnTourAPI.MAP_X.setValue(mapX);
-		EnumParameterOnTourAPI.MAP_Y.setValue(mapY);
-		EnumParameterOnTourAPI.RADIUS.setValue(radius);
-
-		url += EnumParameterOnTourAPI.MAP_X.getKey() + "=" + EnumParameterOnTourAPI.MAP_X.getValue();
-		url += "&";
-		url += EnumParameterOnTourAPI.MAP_Y.getKey() + "=" + EnumParameterOnTourAPI.MAP_Y.getValue();
-		url += "&";
-		url += EnumParameterOnTourAPI.RADIUS.getKey() + "=" + EnumParameterOnTourAPI.RADIUS.getValue();
-
-		EnumParameterOnTourAPI.MAP_X.setValue(null);
-		EnumParameterOnTourAPI.MAP_Y.setValue(null);
-		EnumParameterOnTourAPI.RADIUS.setValue(null);
+	public List<Object> selectListOnLocationBasedInfo(String mapX, String mapY, String radius, String pageNo) {
+		String url = batchSettingUrl(EnumURLOnTourAPI.SELECT_LOCATION_BASED_LIST, mapX, mapY, radius, pageNo);
 		
 		// url 출력
 		logger.info(url);
@@ -61,7 +44,19 @@ public class MenuServiceImpl implements MenuService {
 		return parseJson(response);
 	}
 	
-	private String batchSettingUrl(EnumURLOnTourAPI urlType) {
+	// 페이지 이동 
+	@Override
+	public LinkedHashMap<String, Object> selectPageMovement(String mapX, String mapY, String radius, String pageNo) {
+		String url = batchSettingUrl(EnumURLOnTourAPI.SELECT_LOCATION_BASED_LIST, mapX, mapY, radius, pageNo);
+		
+		// url 출력
+		logger.info(url);
+				
+		String response = useApiAboutTourAPI(url);
+		return arrangeJson(response);
+	}
+	
+	private String batchSettingUrl(EnumURLOnTourAPI urlType, String mapX, String mapY, String radius, String pageNo) {
 
 		/**
 		 * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
@@ -82,7 +77,26 @@ public class MenuServiceImpl implements MenuService {
 				url += "&";
 			}
 		}
-
+		
+		// 위도, 경도, 반경 설정, 한 페이지의 결과 수
+		EnumParameterOnTourAPI.MAP_X.setValue(mapX);
+		EnumParameterOnTourAPI.MAP_Y.setValue(mapY);
+		EnumParameterOnTourAPI.RADIUS.setValue(radius);
+		EnumParameterOnTourAPI.PAGE_NO.setValue(pageNo);
+		
+		url += EnumParameterOnTourAPI.MAP_X.getKey() + "=" + EnumParameterOnTourAPI.MAP_X.getValue();
+		url += "&";
+		url += EnumParameterOnTourAPI.MAP_Y.getKey() + "=" + EnumParameterOnTourAPI.MAP_Y.getValue();
+		url += "&";
+		url += EnumParameterOnTourAPI.RADIUS.getKey() + "=" + EnumParameterOnTourAPI.RADIUS.getValue();
+		url += "&";
+		url += EnumParameterOnTourAPI.PAGE_NO.getKey() + "=" + EnumParameterOnTourAPI.PAGE_NO.getValue();
+		
+		EnumParameterOnTourAPI.MAP_X.setValue(null);
+		EnumParameterOnTourAPI.MAP_Y.setValue(null);
+		EnumParameterOnTourAPI.RADIUS.setValue(null);
+		EnumParameterOnTourAPI.PAGE_NO.setValue(null);
+				
 		return url;
 	}
 	
@@ -154,10 +168,13 @@ public class MenuServiceImpl implements MenuService {
 
 			// body 내부 추출
 			linkedMap = (LinkedHashMap<String, Object>) linkedMap.get("body");
-
+			
+			/** [ totalCount 획득 ] **/
+			String totalCount = String.valueOf(linkedMap.get("totalCount"));
+						
 			// items 내부 추출
 			linkedMap = (LinkedHashMap<String, Object>) linkedMap.get("items");
-
+			
 			// -- 실질적인 필요한 내용
 			// item 내부의 내용 List<Map<String, String>> 형태로 받기
 			parseData = (ArrayList<Map<String, String>>) linkedMap.get("item");
@@ -180,6 +197,7 @@ public class MenuServiceImpl implements MenuService {
 				String readCount = String.valueOf(element.get("readcount")); 			// 조회수
 				String contentTypeId = String.valueOf(element.get("contenttypeid")); 	// 관광타입
 				
+				
 				LocationBasedData locationBasedData = new LocationBasedData();
 				locationBasedData.setTitle(title);
 				locationBasedData.setAddr(addr);
@@ -190,6 +208,7 @@ public class MenuServiceImpl implements MenuService {
 				locationBasedData.setMapY(mapY);
 				locationBasedData.setReadCount(readCount);
 				locationBasedData.setContentTypeId(contentTypeId);
+				locationBasedData.setTotalCount(totalCount);
 				
 				// item 추가
 				dataList.add(locationBasedData);
@@ -200,5 +219,47 @@ public class MenuServiceImpl implements MenuService {
 		}
 
 		return dataList;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private LinkedHashMap<String, Object> arrangeJson(String paramResponse){
+		mapDatas = paramResponse.getBytes();
+		objectMapper = new ObjectMapper();
+
+		// reference :
+		// http://www.mkyong.com/java/jackson-was-expecting-double-quote-to-start-field-name/
+		objectMapper.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+		Map<String, Object> map = new HashMap<String, Object>();
+		LinkedHashMap<String, Object> linkedMap = null;
+		
+		try {
+			map = objectMapper.readValue(mapDatas, HashMap.class);
+			
+			// response 내부 추출
+			linkedMap = (LinkedHashMap<String, Object>) map.get("response");
+
+			// body 내부 추출
+			linkedMap = (LinkedHashMap<String, Object>) linkedMap.get("body");
+			
+			/** [ totalCount 획득 ] **/
+			String totalCount = String.valueOf(linkedMap.get("totalCount"));
+						
+			// items 내부 추출
+			/** 
+			 * {item=
+			 * [
+			 * {addr1=서울특별시 종로구 세종대로 175, addr2=(세종로), areacode=1, booktour=0, cat1=A02, cat2=A0205, cat3=A02050400, contentid=1364932, contenttypeid=12, createdtime=20110817131753, dist=707, firstimage=http://tong.visitkorea.or.kr/cms/resource/92/1056592_image2_1.jpg, firstimage2=http://tong.visitkorea.or.kr/cms/resource/92/1056592_image3_1.jpg, mapx=126.9768376800, mapy=37.5721757834, mlevel=6, modifiedtime=20180125103230, readcount=78129, sigungucode=23, title=세종대왕 동상}, 
+			 * {addr1=서울특별시 종로구 세종대로 175, addr2=(세종로), areacode=1, booktour=0, cat1=A02, cat2=A0205, cat3=A02050400, contentid=1364975, contenttypeid=12, createdtime=20110817135657, dist=707, firstimage=http://tong.visitkorea.or.kr/cms/resource/09/1568209_image2_1.jpg, firstimage2=http://tong.visitkorea.or.kr/cms/resource/09/1568209_image3_1.jpg, mapx=126.9768376800, mapy=37.5721757834, mlevel=6, modifiedtime=20180125103411, readcount=73564, sigungucode=23, title=충무공 이순신 동상}, {addr1=서울특별시 종로구 인사동길 62, addr2=(관훈동) 일대, areacode=1, booktour=0, cat1=A02, cat2=A0203, cat3=A02030600, contentid=264353, contenttypeid=12, createdtime=20021218141629, dist=664, firstimage=http://tong.visitkorea.or.kr/cms/resource/62/2363662_image2_1.jpg, firstimage2=http://tong.visitkorea.or.kr/cms/resource/62/2363662_image3_1.jpg, mapx=126.9833758804, mapy=37.5754310780, mlevel=6, modifiedtime=20170419173334, readcount=73439, sigungucode=23, tel=02-734-0222, title=인사동}, {addr1=서울 종로구 삼청동, addr2=~ 성북구 성북동, areacode=1, booktour=0, cat1=A02, cat2=A0203, cat3=A02030600, contentid=250147, contenttypeid=12, createdtime=20060731000000, dist=797, firstimage=http://tong.visitkorea.or.kr/cms/resource/30/610730_image2_1.jpg, firstimage2=http://tong.visitkorea.or.kr/cms/resource/30/610730_image3_1.jpg, mapx=126.9821035184, mapy=37.5844522286, mlevel=6, modifiedtime=20170419173420, readcount=69061, sigungucode=23, title=삼청동길}
+			 * ]
+			 * } 
+			 * **/
+			linkedMap = (LinkedHashMap<String, Object>) linkedMap.get("items");
+			linkedMap.put("totalCount", totalCount);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return linkedMap;
 	}
 }
